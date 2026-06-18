d427c8a0e00be461488c913d18c1320aabbf51b6
Nitin <your-email@example.com>
2026-06-18 07:11:45 +0530

    Improve portfolio design and animations

--- frontend/script.js
+++ frontend/script.js
@@ -26,3 +26,14 @@ fetch("https://resume-backend-43zb.onrender.com/api/profile")
 document.getElementById("themeBtn").addEventListener("click", () => {
     document.body.classList.toggle("dark");
 });
+const cards = document.querySelectorAll(".card");
+
+window.addEventListener("scroll", () => {
+    cards.forEach(card => {
+        const top = card.getBoundingClientRect().top;
+
+        if (top < window.innerHeight - 100) {
+            card.classList.add("show");
+        }
+    });
+});
